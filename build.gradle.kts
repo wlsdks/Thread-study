8f6d83f591fb174e56373048d898384ae1a701c4
wlsdks <wlsdks12@naver.com>
2024-09-14 14:38:57 +0900

    :sparkles: 코루틴 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,15 @@ repositories {
 }
 
 dependencies {
+	// spring boot
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// coroutines
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+	// test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
