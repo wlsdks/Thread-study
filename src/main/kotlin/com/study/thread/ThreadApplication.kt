cdb066628c1e5b89f80f417f74b71ac1f9844ba9
wlsdks <wlsdks12@naver.com>
2024-09-14 14:30:31 +0900

    :sparkles: 프로젝트 세팅

---
+++ src/main/kotlin/com/study/thread/ThreadApplication.kt
@@ -0,0 +1,11 @@
+package com.study.thread
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThreadApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThreadApplication>(*args)
+}
